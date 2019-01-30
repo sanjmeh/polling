d299c3cd20f595db1ceb90a970ba9a643f47f1d4
Sanjay Mehrotra <sanjmeh@gmail.com>
2019-01-30 14:32:36 +0530

    added css

--- app.R
+++ app.R
@@ -9,9 +9,13 @@ library(data.table)
 library(ggplot2)
 
 ui<- fluidPage(
+  tags$head(
+    tags$link(type="text/css", rel="stylesheet", href="main.css")
+    ),
   textInput("random",label = "Enter your word"),
   actionBttn("send",label = "SEND"), br(),br(),
-  textOutput("current",h1)
+  textOutput("current"),
+  actionBttn("exit","Exit App",icon = icon("sign-out-alt"),style = "material-circle",color = "danger")
                )
 
 server<- function(input,output,session){
@@ -21,6 +25,7 @@ server<- function(input,output,session){
   )
   contents <- reactiveFileReader(intervalMillis = 200,filePath = "test.csv",readFunc = fread,header=T,session = session)
   output$current <- renderText(contents()$phrases %>% dQuote())
+  observeEvent(input$exit,stopApp(1))
 }
 
 shinyApp(ui,server)
